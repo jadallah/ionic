b0d51a938d7691e012f0d02d51ef3f97f6ccb959
jadallah <issa159357@gmail.com>
2026-02-21 12:03:25 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+(function($){
+	$(document).ready(function() {	
+
+		
+		$('#volumeButton').click(function() {
+			const icon = $('#volumeIcon');
+			icon.toggleClass('fa-volume-high fa-volume-xmark');
+		  });
+
+		  $('.toggle-btn button').click(function() {
+			$('.nav-wrapper').toggleClass('nav-active');
+			const icon = $(this).find('i');
+			icon.toggleClass('fa-bars fa-xmark');
+		  });
+	
+		  $(document).click(function(e) {
+			if (!$(e.target).closest('.toggle-btn, .nav-wrapper').length) {
+			  $('.nav-wrapper').removeClass('nav-active');
+			  $('.toggle-btn button i').removeClass('fa-xmark').addClass('fa-bars');
+			}
+		  });
+				
+		
+		
+		
+		
+		
+		
+		
+		
+	});
+})(jQuery);
